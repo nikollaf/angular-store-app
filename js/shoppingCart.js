63ec8267918d3ba67405a1a5df0d9d6be197c6f9
nikolla <franc.nikolla@gmail.com>
2013-08-03 11:49:50 -0500

    Added Bootstrap

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -1,6 +1,28 @@
 shoppingCart.prototype.checkout = function(serviceName, clearCart) {
 	// select service
 	if (serviceName = null) {
-		var p = this.checkoutParameters[Object.key]
+		var p = this.checkoutParameters[Object.keys(this.checkoutParameters)[0]];
+		serviceName = p.serviceName;
+	}
+
+	if (serviceName == null) {
+		throw "Define at least one checkout service.";
+	}
+
+	var parms = this.checkoutParameters[serviceName];
+	if (parms == null) {
+		throw "Cannot get checkout parameters for ' " + serviceName + "'.";
+	}
+
+	// invoke service
+	switch (parms.serviceName) {
+		case "PayPal" :
+			this.checkoutPayPal(parms, clearCart);
+			break;
+		case "Google":
+			this.checkoutGoogle(parms, clearCart);
+			break;
+		default: 
+			throw "Unknown checkout service : " + parms.serviceName;
 	}
 }
